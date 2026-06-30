02d15aa078a9a8fd7be1488acd1f6c88a62745dc
Anonima1701 <brendagabri1701@gmail.com>
2026-06-30 00:51:39 -0300

    Added function to save input to file.

--- main.c
+++ main.c
@@ -2,9 +2,10 @@
 #include <stdlib.h>
 #include <locale.h>
 #include "veiculo.h"
+#include "salvar.h"
 
 void linha () {
-  printf("=≈==================");
+  printf("======================================\n");
 }
 
 //Função principal
@@ -14,27 +15,32 @@ int main() {
   //Variaveis
   Veiculo v;
   int escolha;
+  VerificarExistencia();
   
   do {
       //Atualizar valor
       int valor1;
       
       // Estrutura do Menu
+      system("clear");
       linha();
       printf("       Menu Principal         \n");
-      printf("%d", valor1);
+      linha();
+      exibirVeiculo(&v);
+      printf("\n");
       linha();
       printf("1. Cadastrar Veículo\n");
-      printf("2. Exibir Veículo\n");
-      printf("3. Receber valor\n");
-      printf("4. Exit\n");
+
+      //printf("2. Exibir Veículo\n");
+
+      printf("2. Exit\n");
       linha();
       printf("Escreva sua escolha: ");
   
       // Verificação de escolha
       if (scanf("%d", &escolha) != 1) {
           printf("\n[Erro] Opção invalida. Por favor, escolha um número.\n");
-          clearInputBuffer();
+          stdin = freopen(NULL,"r",stdin);
           continue; 
       }
   
@@ -45,24 +51,25 @@ int main() {
               printf("\n-> Iniciando Cadastro do Veículo\n");
               cadastrarVeiculo(&v);
               break;
+
+          /*
           case 2 :
               printf("\n-> Exibindo informações do veículo\n");
               exibirVeiculo(&v);
               break;
-          case 3:
-              printf("\n-> Valor: \n");
-              scanf("%d", &valor1);
-              break;
-          case 4:
+          */
+
+          case 2:
+              system("clear");
               printf("\nFinalizando tarefa!\n");
+              CriarArquivo (&v);
               break;
           default:
               printf("\n[Aviso] Opção incorreta! Tente novamente.\n");
       }
-
       printf("\n");
 
-  } while (escolha != 4);
+  } while (escolha != 2);
 
 return 0;
 
