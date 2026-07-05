f94660cfae6f1ff5aecce52575a41efa6c9c155e
Anonima1701 <brendagabri1701@gmail.com>
2026-07-05 17:53:18 -0300

    Arrumando verificação e colorindo

--- linha.c
+++ linha.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 #include "linha.h"
 
+#define ANSI_COLOR_CYAN    "\x1b[36m"
+#define ANSI_COLOR_RESET   "\x1b[0m"
+
 void linha () {
-    printf("======================================\n");
+    printf(ANSI_COLOR_CYAN "======================================\n" ANSI_COLOR_RESET);
 }
--- main.c
+++ main.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <locale.h>
+
 #include "veiculo.h"
 #include "arquivo.h"
 #include "relatorio.h"
@@ -10,6 +11,13 @@
 #include "menu.h"
 #include "linha.h"
 
+
+#define ANSI_COLOR_RED     "\x1b[31m"
+#define ANSI_COLOR_GREEN   "\x1b[32m"
+#define ANSI_COLOR_YELLOW  "\x1b[33m"
+#define ANSI_COLOR_CYAN    "\x1b[36m"
+#define ANSI_COLOR_RESET   "\x1b[0m"
+
 //Função principal
 int main() {
     setlocale(LC_ALL, "Portuguese_Brazil"); //para ser aceito caracteres especiais.
@@ -17,7 +25,7 @@ int main() {
 
     //Variaveis
     Veiculo v;
-    int escolha;
+    int escolha = 0;
     Condutor c;
     Cotacao *lista = NULL;
     int total = 0;
@@ -32,52 +40,52 @@ int main() {
         // Estrutura do Menu
 
         linha();
-        printf("         Menu Principal        \n");
+        printf(ANSI_COLOR_GREEN"         Menu Principal        \n"ANSI_COLOR_RESET);
         linha();
-        printf("1. Cadastro (Veiculo,condutor e calculo de premio)\n");
+        printf(ANSI_COLOR_YELLOW "1. Cadastro (Veiculo,condutor e calculo de premio)\n" ANSI_COLOR_RESET);
         linha();
-        printf("2. Relatorios\n");
+        printf(ANSI_COLOR_YELLOW "2. Relatorios\n"ANSI_COLOR_RESET);
         linha();
-        printf("3. Exit\n");
+        printf(ANSI_COLOR_YELLOW "3. Exit\n" ANSI_COLOR_RESET);
         linha();
-        printf("Escreva sua escolha: ");
+        printf(ANSI_COLOR_GREEN "Escreva sua escolha: "ANSI_COLOR_RESET);
 
         // Verificação de escolha
         if (scanf("%d", &escolha) != 1) {
-            printf("Digite apenas numeros!\n");
-        } else if (escolha < 1 || escolha > 3) {
-            printf("Opcao invalida!\n");
-        } else {
-            printf("Opcao escolhida: %d\n", escolha);
-        }
+            printf(ANSI_COLOR_RED "Digite apenas numeros!\n" ANSI_COLOR_RESET);
+
+         while (getchar() != '\n'); 
+            escolha = -1; 
+            }
 
         // Opções a serem usadas
         switch (escolha) {
         case 1:
             system("clear");
-            printf("\n-> Abrindo Menu de Cadastro\n");
+            printf(ANSI_COLOR_GREEN "\n-> Abrindo Menu de Cadastro\n" ANSI_COLOR_RESET);
             menuCadastro(&v, &c, &lista, &total);
             break;
 
         case 2 :
             system("clear");
-            printf("\n-> Abrindo Menu de Relatórios\n");
+            printf(ANSI_COLOR_GREEN "\n-> Abrindo Menu de Relatórios\n" ANSI_COLOR_RESET);
             menuRelatorio(lista, total);
             break;
 
         case 3:
             system("clear");
-            printf("\n-> Finalizando tarefa!\n");
+            printf(ANSI_COLOR_YELLOW "\n-> Finalizando tarefa!\n" ANSI_COLOR_RESET);
             break;
 
         default:
             system("clear");
-            printf("\n[Aviso] Opção incorreta! Tente novamente.\n");
+            printf(ANSI_COLOR_RED "\n[Aviso] Opção incorreta! Tente novamente.\n" ANSI_COLOR_RESET);
         }
 
         printf("\n");
 
     } while (escolha != 3);
+
     free(lista);
     return 0;
 }
