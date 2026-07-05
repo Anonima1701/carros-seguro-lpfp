1f798542a031919583066c22491569c312f5e026
Anonima1701 <brendagabri1701@gmail.com>
2026-07-04 21:15:42 -0300

    arrumei erro de compilação de novo

--- relatorio.h
+++ relatorio.h
@@ -1,9 +1,7 @@
-//Relatorio 
-#ifndef RELATORIO_H 
+#ifndef RELATORIO_H
 #define RELATORIO_H
 
-
-//Funcoes para o relatorio
+#include "cotacao.h"
 
 void Listar_cotacoes(Cotacao *lista, int total);
 void filtrar_por_condutor(Cotacao *lista, int total, char *nome);
